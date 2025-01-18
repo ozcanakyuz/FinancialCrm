bb598c0b801655ca7181d98e4903e6a42e88c468
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-18 12:46:40 +0300

    screenshots OK.

--- FinancialCrm/FrmSpendings.Designer.cs
+++ FinancialCrm/FrmSpendings.Designer.cs
@@ -118,9 +118,9 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.Size = new System.Drawing.Size(64, 19);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Kategoriler";
+            this.label1.Text = "Giderler";
             // 
             // btnCikisYap
             // 
